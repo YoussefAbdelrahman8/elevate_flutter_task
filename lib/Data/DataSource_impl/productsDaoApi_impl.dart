e164cd1d29184f1b0fe1009cf0afc33b26b092ec
Youssef Ashraf Othman Abdelrahman <youssef.abdelrahman158@gmail.com>
2024-12-27 01:37:31 +0200

    + set environment

---
+++ lib/Data/DataSource_impl/productsDaoApi_impl.dart
@@ -0,0 +1,5 @@
+import 'package:elevate_flutter_task/Data/DataSource/productsDao.dart';
+
+class ProductsDaoApiImpl extends ProductsDao{
+
+}
---
+++ lib/UI/ProductsCatalogScreen/ProductsCatalogScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProductsCatalogScreen extends StatelessWidget {
+      static const routeName = "home";
+  const ProductsCatalogScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
