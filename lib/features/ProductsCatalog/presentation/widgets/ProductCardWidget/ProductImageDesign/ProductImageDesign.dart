74ab30221c7c241475359affc2744ade3e94f18c
Youssef Ashraf Othman Abdelrahman <youssef.abdelrahman158@gmail.com>
2024-12-29 08:16:11 +0200

    + Product Catalog logic

--- lib/features/ProductsCatalog/presentation/widgets/ProductCardWidget/ProductImageDesign/ProductImageDesign.dart
+++ lib/features/ProductsCatalog/presentation/widgets/ProductCardWidget/ProductImageDesign/ProductImageDesign.dart
@@ -1,25 +1,32 @@
+import 'package:elevate_flutter_task/core/resources/color_manager.dart';
 import 'package:flutter/material.dart';
 import 'ProductImage.dart';
 
 class ProductImageDesign extends StatelessWidget {
-  const ProductImageDesign({super.key});
+  final String image;
+
+  const ProductImageDesign({super.key, required this.image});
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       alignment: Alignment.topRight,
       children: [
-        const ProductImage(),
+        ProductImage(
+          image: image,
+        ),
         Padding(
           padding: const EdgeInsets.all(5.0),
           child: CircleAvatar(
-            backgroundColor: Colors.white,
+            backgroundColor: ColorManager.lightTertiary,
             radius: 18,
             child: Center(
               child: IconButton(
                 onPressed: () {},
-                icon:  ImageIcon(
-                  AssetImage("assets/images/..png",),
+                icon: ImageIcon(
+                  AssetImage(
+                    "assets/images/..png",
+                  ),
                   color: Theme.of(context).colorScheme.onPrimary,
                 ),
               ),
